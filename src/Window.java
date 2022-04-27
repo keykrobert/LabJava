2991f7589074364f8ef3ae281de07af2d3fd76de
Robert <keykrobert2@gmail.com>
2022-04-27 08:15:29 +0300

    [Lab-3] Laboratorium 3

---
+++ src/Window.java
@@ -0,0 +1,19 @@
+class Window extends Rectangle {
+
+    public Window(int sideOne, int sideTwo, int sideThree) {
+        super(sideOne, sideTwo, sideThree);
+    }
+}
+
+class Student extends Window {
+    public Student(int sideOne, int sideTwo, int sideThree) {
+        super(sideOne, sideTwo, sideThree);
+    }
+}
+
+class Person extends Window {
+    public Person(int sideOne, int sideTwo, int sideThree) {
+        super(sideOne, sideTwo, sideThree);
+    }
+}
+
